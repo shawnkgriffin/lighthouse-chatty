7c4ed78602274a8924001a72e19cd64b438218bb
Shawn Griffin <shawn@shawngriffin.com>
2017-11-02 00:06:05 +0000

    added postMessage to outgoing messages, accept 'incomingMessage'

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ class App extends Component {
     this.state = {
       connected: false,
       currentUser: { name: '' },
-      messages: [
-      ]
+      messages: []
     };
 
     this.onNewMessage = this.onNewMessage.bind(this);
@@ -18,20 +17,40 @@ class App extends Component {
 
   componentDidMount() {
     this.WebSocket = new WebSocket('ws://localhost:3001/');
+
+    // Will use this for a connected icon
     this.setState({ connected: true });
+
+    //incoming message
     this.WebSocket.onmessage = event => {
       console.log('received', event.data);
       const newMessage = JSON.parse(event.data);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({
-        messages: this.state.messages.concat([
-          {
-            username: newMessage.username,
-            content: newMessage.content
-          }
-        ])
-      });
+
+      // There are different types of messages. We need to route on them.
+      // The socket event data is encoded as a JSON string.
+      // This line turns it into an object
+      const data = JSON.parse(event.data);
+      switch (data.type) {
+        case 'incomingMessage':
+          // Update the state of the app component.
+          // Calling setState will trigger a call to render() in App and all child components.
+          this.setState({
+            messages: this.state.messages.concat([
+              {
+                id: newMessage.id,
+                username: newMessage.username,
+                content: newMessage.content
+              }
+            ])
+          });
+          break;
+        case 'incomingNotification':
+          // handle incoming notification
+          break;
+        default:
+          // show an error in the console if the message type is unknown
+          throw new Error('Unknown event type ' + data.type);
+      }
     };
   }
 
@@ -39,7 +58,7 @@ class App extends Component {
   onNewMessage(username, content) {
     // Send the msg object as a JSON-formatted string.
     const newMessage = {
-      type : 'postMessage',
+      type: 'postMessage',
       username: username,
       content: content
     };
